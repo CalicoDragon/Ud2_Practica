7ef7e37c43350d91c2127fc388a2ad23ccb74c4d
Sofia Gilsanz <softie.gd@gmail.com>
2025-11-06 19:17:05 +0100

    finished

--- app.js
+++ app.js
@@ -12,18 +12,8 @@ const API_PORT = process.env.API_PORT || 3000;
 const app = express();
 
 // Middleware
-// app.use(cors);
-
-// Source - https://stackoverflow.com/questions/79604374/app-useexpress-json-is-not-working-in-express-version-express5-1-0-but-wo
-// Posted by Sakar
-// Retrieved 2025-11-06, License - CC BY-SA 4.0
-
-app.use((req, res, next) => {
-  express.json()(req, res, (err) => {
-    if (!req.body) req.body = {};
-    next(err);
-  });
-});
+app.use(cors());
+app.use(express.json());
 
 // Routes
 app.use("/api", require("./api/routes"));
