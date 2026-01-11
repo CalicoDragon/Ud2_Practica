7c53ad1c790aca4330feb16306197834bae29210
Sofia Gilsanz <softie.gd@gmail.com>
2026-01-11 19:38:20 +0100

    slack logging

---
+++ api/config/winstonLogger.config.js
@@ -0,0 +1,26 @@
+// Import
+const { createLogger, format, transports } = require("winston");
+const { MESSAGE } = require("triple-beam");
+const { combine, timestamp, label } = format;
+
+// Formats
+const generalFormat = format((info) => {
+  const { level, message, timestamp } = info;
+  info[MESSAGE] = `LOG (${timestamp}) ${level}: ${message}`;
+  return info;
+});
+
+// Logger
+const appLogger = createLogger({
+  format: combine(
+    label({ label: "APP", message: true }),
+    timestamp(),
+    generalFormat()
+  ),
+  transports: [new transports.Console()],
+});
+
+// Export
+module.exports = {
+  appLogger,
+};
--- app.js
+++ app.js
@@ -5,11 +5,34 @@ require("dotenv").config();
 
 const mongooseDBConnect = require("./api/config/mongodb.config");
 
+const morganBody = require("morgan-body");
+const { IncomingWebhook } = require("@slack/webhook");
+const { INTERNAL_SERVER_ERROR } = require("./api/utils/handleResponse.util");
+
 // Consts
 const API_PORT = process.env.API_PORT || 3000;
+const SLACK_WEBHOOK = process.env.SLACK_WEBHOOK;
+
+// Webhook y Logger Stream
+const webhook = new IncomingWebhook(SLACK_WEBHOOK);
+
+const loggerStream = {
+  write: (message) => {
+    webhook.send({ text: message });
+  },
+};
 
 // Start server
 const app = express();
+
+// Le paso la app para que intercepte y salte errores con codigo menor al server
+morganBody(app, {
+  noColors: true,
+  skip: function (req, res) {
+    return res.statusCode < INTERNAL_SERVER_ERROR;
+  },
+  Stream: loggerStream,
+});
 
 // Middleware
 app.use(cors());
